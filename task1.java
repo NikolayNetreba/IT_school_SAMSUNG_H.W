b70fcdcf2480b88917033a41deddc822a0c3257c
NikolayNetreba <114187851+NikolayNetreba@users.noreply.github.com>
2022-09-27 17:44:02 +0300

    Delete task1.java

--- task1.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class task1 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        System.out.println(a/100 + a % 10 + a / 10 % 10);
-        System.out.println(a+((a + 1) % 2 + 1));
-    }
-}
