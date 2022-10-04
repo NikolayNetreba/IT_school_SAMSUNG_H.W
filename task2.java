25173bec4b38da5068c36316d34c614890d6b8bb
NikolayNetreba <114187851+NikolayNetreba@users.noreply.github.com>
2022-10-04 23:24:10 +0300

    Update task2.java

--- task2.java
+++ task2.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
+
 public class task2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String x = in.nextLine();
-        String[] x1 = x.split("");
-        for(int i = x1.length - 1; i >= 0; --i){
-            if(x1[i] != "0" && i != x1.length - 1  )
-                System.out.print(x1[i]);}
-
-
-
+        long n = in.nextLong();
+        long res = 0;
+        while (Math.abs(n) > 0) {
+            res *= 10;
+            res += n % 10;
+            n /= 10;
+        }
+        System.out.println(res);
     }
 }
