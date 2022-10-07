79de1c01db3b2a9e4261c6aee4f651f6476b500f
NikolayNetreba <114187851+NikolayNetreba@users.noreply.github.com>
2022-10-07 17:13:37 +0300

    .

--- task3.java
+++ task3.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+
 
 public class task3 {
     public static void main(String args[]){
-        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n; ++i) array[i] = in.nextInt();
+        int min = array[0];
+        for(int i = 1; i < n; ++i){
+            if(array[i] > 0 && array[i] < min || min < 0){
+                min = array[i];
+            }
+        }
+        System.out.println(min);
     }
 
 
---
+++ task3_2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class task3_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int min = -1;
+        for(int i = 1;;++i){}
+    }
+}
