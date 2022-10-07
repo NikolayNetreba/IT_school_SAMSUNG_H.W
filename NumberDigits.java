df696339373b8ecc28a0ddffef40117c617a69ab
NikolayNetreba <114187851+NikolayNetreba@users.noreply.github.com>
2022-10-07 21:57:16 +0300

    .

--- NumberDigits.java
+++ NumberDigits.java
@@ -1,8 +1,25 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class NumberDigits {
-
+    public static void SumOfSeven(int a, int b){
+        int sum = 0, n;
+        for(int i = a; i < b + 1; ++i){
+            if(i % 7 == 0 && (i % 100 / 10 != 0)){
+                n = i;
+                while(n != 0){
+                    int last = n % 10;
+                    sum += last;
+                    n /= 10;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        SumOfSeven(a, b);
 
 
 
