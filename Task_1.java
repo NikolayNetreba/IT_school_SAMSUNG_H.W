d3e04cec1ce4916d473f68c3336c6f58d8e29fb8
NikolayNetreba <114187851+NikolayNetreba@users.noreply.github.com>
2022-10-23 20:59:24 +0300

    ,

---
+++ Task_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        byte[] a = new byte[n / 8 + 1];
+        for (int i = 0; i < n; ++i) {
+            byte x = (byte) (in.nextBoolean() ? 1 : 0);
+            a[i / 8] = (byte) (a[i / 8] | (x << (i % 8)));
+        }
+        for (int i = 0; i < n; ++i) {
+            System.out.print(((a[i / 8] & (1 << (i % 8))) != 0) + " ");
+        }
+    }
+}
