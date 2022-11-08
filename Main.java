7b63f530311da5ae38ea371d4dce033f6b88e0e9
NikolayNetreba <114187851+NikolayNetreba@users.noreply.github.com>
2022-11-08 20:55:47 +0300

    /

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        Fraction fraction1 = new Fraction(4, 5);
+
+
+        Fraction num = new Fraction(4);
+        System.out.println(num);
+
+    }
+}
